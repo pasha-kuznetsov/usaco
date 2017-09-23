b107704ab3df0cc8ac5c9da87353f4a19dda031f
Pasha Kuznetsov <pasha@tydbits.com>
2017-09-22 20:45:59 -0500

    USACO 2017 Feb 1 MinCross: input/output

--- src/main/java/usaco_2017_feb_1/MinCross.java
+++ src/main/java/usaco_2017_feb_1/MinCross.java
@@ -2,16 +2,30 @@ package usaco_2017_feb_1;
 
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MinCross {
     public static void main(String [] args) throws FileNotFoundException {
-        Scanner in = new Scanner("mincross.in");
-        PrintStream out = new PrintStream("mincross.out");
+        main(new Scanner("mincross.in"), new PrintStream("mincross.out"));
     }
 
-    static int minCrossingPairs(List<Integer> fields) {
+    static void main(Scanner in, PrintStream out) {
+        int n = in.nextInt();
+        List<Integer> fields1 = readFields(in, n);
+        List<Integer> fields2 = readFields(in, n);
+        out.print(minCrossingPairs(fields1, fields2));
+    }
+
+    private static List<Integer> readFields(Scanner in, int n) {
+        List<Integer> fields = new ArrayList<>(n);
+        for (int i = 0; i < n; ++i)
+            fields.add(in.nextInt());
+        return fields;
+    }
+
+    static int minCrossingPairs(List<Integer> fields1, List<Integer> fields2) {
         return 0;
     }
 }
--- src/test/groovy/usaco_2017_feb_1/MinCross_Test.groovy
+++ src/test/groovy/usaco_2017_feb_1/MinCross_Test.groovy
@@ -3,14 +3,34 @@ package usaco_2017_feb_1
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import java.nio.charset.StandardCharsets
+
 class MinCross_Test extends Specification {
     @Unroll
     def "test #input"() {
-        expect:
-        MinCross.minCrossingPairs(input) == expectedOutput
+        when:
+        def io = new InputOutput(input)
+        MinCross.main(io.input, io.print)
+
+        then:
+        io.output() == expectedOutput
 
         where:
         input | expectedOutput
-        [5, 5, 4, 1, 3, 2, 1, 3, 2, 5, 4] | 0
+        '5 5 4 1 3 2 1 3 2 5 4' | '0'
+    }
+
+    static class InputOutput {
+        Scanner input
+        OutputStream output = new ByteArrayOutputStream()
+        PrintStream print = new PrintStream(output, true)
+
+        InputOutput(String input) {
+            this.input = new Scanner(new StringReader(input))
+        }
+
+        String output() {
+            return output.toString()
+        }
     }
 }
